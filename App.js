2167e7db741b25a8fe9e9ca299cca57fa910233b
shalinikashyap08 <shalini.kashyap@chicmic.co.in>
2026-01-13 12:26:19 +0530

    Creating child elements in DOM changes

--- App.js
+++ App.js
@@ -5,4 +5,26 @@ const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+// root.render(heading);
+
+/**
+<div id="parent">
+    <div id="child">
+        <h1>I am a h1 tag.</h1>
+        <h2>I am a h1 tag.</h2>
+    </div>
+</div>
+*/
+
+let child1 = React.createElement("div", {id: "child1"},
+        [React.createElement("h1", {}, "I am a h1 tag."),React.createElement("h2", {}, "I am a h2 tag.")])
+let child2 =React.createElement("div", {id: "child2"},
+        [React.createElement("h1", {}, "I am a h1 tag."),React.createElement("h2", {}, "I am a h2 tag.")])
+
+let parent = React.createElement("div", {id: "parent"}, 
+        [child1,child2]
+    )
+
+
+
+root.render(parent);
