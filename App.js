5bfce369c2399774dd65396b2887fde4d6d06c17
shalinikashyap08 <shalini.kashyap@chicmic.co.in>
2026-01-16 23:14:31 +0530

    Different ways of rendering dynamic content in JSX

--- App.js
+++ App.js
@@ -5,9 +5,13 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const Title = () => (<h1 className="title">React Functional component</h1>);
 // component composition
+let dynamicValue = 'This value is dynamic';
 const HeadingComponent = () => (
     <div className="container">
-        <Title />
+        {Title()}
+        <Title></Title>
+        <Title/>
+        <h6>{dynamicValue}</h6>
         <h2 className="heading">Functions returning JSX or React element is functional component</h2>
     </div>
 );
