b4872881ac13a700fa19eb37fca10ed390218dfc
shalinikashyap08 <shalini.kashyap@chicmic.co.in>
2026-01-16 11:23:18 +0530

    JSX usage, adding scripts for npm commands, Integration of Not Rendered section for detecting issue with rendering.

--- App.js
+++ App.js
@@ -1,31 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// createElement returns ReactElement object
-// accepts props :  {id:"heading"}, "Hello World!"
-const heading = React.createElement("h1", { id: "heading" }, "React starts!");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// JSX - HTML or XML like syntax
+const jsxHeading = <h1 id="heading-jsx" className="heading">React and JSX begins</h1>;
 
-root.render(heading);
-
-/**
-<div id="parent">
-    <div id="child">
-        <h1>I am a h1 tag.</h1>
-        <h2>I am a h1 tag.</h2>
-    </div>
-</div>
-*/
-
-// let child1 = React.createElement("div", {id: "child1"},
-//         [React.createElement("h1", {}, "I am a h1 tag."),React.createElement("h2", {}, "I am a h2 tag.")])
-// let child2 =React.createElement("div", {id: "child2"},
-//         [React.createElement("h1", {}, "I am a h1 tag."),React.createElement("h2", {}, "I am a h2 tag.")])
-
-// let parent = React.createElement("div", {id: "parent"}, 
-//         [child1,child2]
-//     )
-
-
-
-// root.render(parent);
+root.render(jsxHeading);
